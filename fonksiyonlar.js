924fd4133eb2b719645cff51fcda6d784b0344d7
Bahadir HG <bahadirhamzagul@gmail.com>
2020-12-06 15:36:37 +0300

    Update fonksiyonlar.js

--- fonksiyonlar.js
+++ fonksiyonlar.js
@@ -2,19 +2,11 @@
 var months = ["January", "February", "March", "April", "May", "June", "July",
          "August", "September", "October", "November", "December"];
 
-
-
-
 var getTimeStamp = function(now){
-
+         
     var Stamp = now.getDate()+"-"+months[now.getMonth()]+"-"+now.getFullYear()+"   "+now.getHours()+":"+now.getMinutes()
      return  Stamp;
-
 } 
-
-
-
-
 
 module.exports = {
      getTimeStamp
